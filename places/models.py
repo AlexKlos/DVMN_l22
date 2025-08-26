f91573391ae0d5b529ffdeffb67c3ba932f84f51
AlexKlos <klos@list.ru>
2025-08-26 14:26:55 +0700

    Улучшение - Разрешите полям оставаться пустыми

--- places/models.py
+++ places/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200, blank=True, verbose_name='Название')
-    description_short = models.CharField(max_length=400, verbose_name='Краткое описание')
-    description_long = models.TextField(verbose_name='Полное описание')
+    description_short = models.CharField(max_length=400, 
+                                         verbose_name='Краткое описание', 
+                                         blank=True)
+    description_long = models.TextField(verbose_name='Полное описание', blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
