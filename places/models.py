753b5e9d111d139831828b8284665f7109adc656
AlexKlos <klos@list.ru>
2025-08-26 14:13:47 +0700

    Улучшение - Укажите русскоязычный verbose_name

--- places/models.py
+++ places/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    description_short = models.CharField(max_length=400)
-    description_long = models.TextField()
-    lng = models.FloatField()
-    lat = models.FloatField()
+    title = models.CharField(max_length=200, blank=True, verbose_name='Название')
+    description_short = models.CharField(max_length=400, verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
 
     def __str__(self):
         return self.title
 
 
 class PlaceImage(models.Model):
-    image = models.ImageField(upload_to='places/')
+    image = models.ImageField(upload_to='places/', verbose_name='Изображение')
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     order = models.PositiveIntegerField(default=0, blank=False, null=False)
 
