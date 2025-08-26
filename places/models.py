4848d5fa47b81daf116a4a082b5daaf87d322925
AlexKlos <klos@list.ru>
2025-08-26 19:58:16 +0700

    Улучшение - Замените поля CharField / TextField

--- places/models.py
+++ places/models.py
@@ -3,9 +3,7 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200, blank=True, verbose_name='Название')
-    short_description = models.CharField(max_length=400, 
-                                         verbose_name='Краткое описание', 
-                                         blank=True)
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
     long_description = models.TextField(verbose_name='Полное описание', blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
